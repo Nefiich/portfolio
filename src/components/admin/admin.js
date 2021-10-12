52d6436aec0544ad574db89bd68116668bc35896
Emin Nefic <emin.nefic@gmail.com>
2021-10-12 14:27:35 +0200

    Adding Admin edit

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -1,96 +1,116 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-function Projects(props){
+function Admin() {
 
     const [data, setData] = useState();
     const [loaded, setLoaded] = useState(false);
-    const content = (
-        <div className="content">
-            <div className="project-text-container">
-                <h2 className="projects-text">Projects</h2>
-            </div>
-            <div className="content-items">
-                <div className="item-button" onClick={props.onClick}>
-                    <div className="colored-dot"></div>
-                    <h3>Portfolio</h3>
-                </div>
-                {
-                    data.map(project =>( 
-                        <div className="item-button">
-                        <div className="colored-dot"></div>
-                        <h3>{project.name}</h3>
-                    </div>
-                    ))
-                }
-            </div>
-        </div>
-    )
-
-    useEffect(async () => {
-        const result = await axios.get(
-          'https://ancient-escarpment-01509.herokuapp.com/api/projects',
-        ).then(function(response) {
-            setData(response.data)
-            setLoaded(true)
-            console.log(response.data)
-        });
-      }, []);
 
 
-      
-    console.log(loaded);
-
-    console.log(data[1])
-    return(
-        <div>
-            {loaded ? content : 'Loading...'}
-       </div>
-    );
-}
-
-
-function Project(props){
-
+    const [id, setId] = useState();
     const [name, setName] = useState();
     const [github, setGithub] = useState();
     const [demo, setDemo] = useState();
     const [bgImage, setBgImage] = useState();
-    return(
-        <div className="content">
-             <div className="project-text-container">
-                 <i class="fas fa-arrow-left" onClick={props.onClick}></i>
-                <h2 className="projects-text" style={{margin: '0'}}>Portfolio</h2>
-            </div>
-            <div className="project-conteiner">
-                <div className="project-input-container">
-                    <h3>Project Name</h3>
-                    <input className="project-input" value={name} onChange={(e) => {setName(e.target.value)}}/>
-                </div>
-                <div className="project-input-container">
-                    <h3>Gihub link</h3>
-                    <input className="project-input" value={github} onChange={(e) => {setGithub(e.target.value)}}/>
-                </div>
-                <div className="project-input-container">
-                    <h3>Demo link</h3>
-                    <input className="project-input" value={demo} onChange={(e) => {setDemo(e.target.value)}}/>
-                </div>
-                <div className="project-input-container">
-                    <h3>Image link</h3>
-                    <input className="project-input" value={bgImage} onChange={(e) => {setBgImage(e.target.value)}}/>
-                    <img src="./images/exploreba.jpg" className="project-img" alt="project"/>
-                </div>
-            </div>
-            <div className="finish-button">
-                <h3>Finish editing</h3>
-            </div>
-        </div>
-    )
-}
-
-function Admin() {
 
     const [flag, setFlag] = useState(false);
+
+    useEffect( () => {
+        axios.get('https://ancient-escarpment-01509.herokuapp.com/api/projects')
+        .then(res => {
+            setData(res.data)
+            setLoaded(true)
+            console.log(res.data)
+        });
+    }, []);
+
+    const getProject = (purpose, id, name, git, demo, img) =>{
+        setFlag(purpose);
+        if(purpose === true){
+            setId(id);
+            setName(name);
+            setGithub(git);
+            setDemo(demo);
+            setBgImage(img);
+        }
+    }
+
+    const editProject = () =>{
+        axios.post(`https://ancient-escarpment-01509.herokuapp.com/api/projects/edit/${id}`, {
+            name: name,
+            github: github,
+            demo: demo,
+            img: bgImage
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+
+    function Projects(props){
+        const content = (
+            <div className="content">
+                <div className="project-text-container">
+                    <h2 className="projects-text" style={{margin: '0'}}>Projects</h2>
+                    <i class="fas fa-plus-circle plus-icon"></i>
+                </div>
+                <div className="content-items">
+
+                    {
+                        loaded ? 
+                        data.map(project =>( 
+                            <div className="item-button" onClick={() => {getProject(true, project.id, project.name, project.github, project.demo, project.img)}}>
+                                <div className="colored-dot"></div>
+                                <h3>{project.name}</h3>
+                            </div>
+                        )) : <h3>Loading</h3>
+                    }
+                </div>
+            </div>
+        )    
+        return(
+            <div style={{width: '100%'}}>
+                {loaded ? content : 'Loading...'}
+            </div>
+        );
+    }
+
+    function Project(props){
+        return(
+            <div className="content">
+                 <div className="project-text-container">
+                    <i class="fas fa-arrow-left back-icon" onClick={() => {getProject(false)}}></i>
+                    <h2 className="projects-text" style={{margin: '0'}}>Portfolio</h2>
+                </div>
+                <div className="project-conteiner">
+                    <div className="project-input-container">
+                        <h3>Project Name</h3>
+                        <input className="project-input" value={name} onChange={(e) => {setName(e.target.value)}}/>
+                    </div>
+                    <div className="project-input-container">
+                        <h3>Gihub link</h3>
+                        <input className="project-input" value={github} onChange={(e) => {setGithub(e.target.value)}}/>
+                    </div>
+                    <div className="project-input-container">
+                        <h3>Demo link</h3>
+                        <input className="project-input" value={demo} onChange={(e) => {setDemo(e.target.value)}}/>
+                    </div>
+                    <div className="project-input-container">
+                        <h3>Image link</h3>
+                        <input className="project-input" value={bgImage} onChange={(e) => {setBgImage(e.target.value)}}/>
+                        <img src="./images/exploreba.jpg" className="project-img" alt="project"/>
+                    </div>
+                </div>
+                <div className="finish-button" onClick={() => {editProject()}}>
+                    <h3>Finish editing</h3>
+                </div>
+            </div>
+        )
+    }
+
     return(
         <div>
             <div className="top-nav">
@@ -111,7 +131,7 @@ function Admin() {
                         </a>
                     </div>
                 </div>
-                {flag ? <Project onClick={() => setFlag(false)}/> : <Projects onClick={() => setFlag(true)}/>}
+                {flag ? Project() : <Projects/>}
             </div>
         </div>
     );
